b6ebfed8a6594ed212aef07ac053279361314ce2
rlakshmik <43289604+rlakshmik@users.noreply.github.com>
2019-03-20 15:06:02 -0700

    Add files via upload

---
+++ employee.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+	selector : 'my-employee' ,
+	templateUrl : './employee.component.html'
+})
+export class EmployeeComponent {
+	 firstName: string = 'Tom';
+    lastName: string = 'Hopkins';
+    gender: string = 'Male';
+    age: number = 20;
+}
